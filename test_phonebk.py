b30bf5b0604f3def0a1a43aeae4ceccbd0af9c79
Moses Oloo <olooine@gmail.com>
2018-03-01 14:17:36 +0300

    task [#155618757] unit test commit

---
+++ test_phonebk.py
@@ -0,0 +1,32 @@
+import unittest
+from phonebk import Phonebook
+
+class PhonebookTestCase(unittest.TestCase):
+
+	def test_add_contacts(self):
+		phonebk = Phonebook()
+		response = phonebk.add_contact('003344', 'Moses')
+		self.assertEqual(response["message"], "contact added successfully")
+		pass
+
+	def test_view_contact(self):
+		phonebk = Phonebook()
+		response = phonebk.view_contacts()
+		self.assertEqual(response['message'], 'you are now viewing the contacts')
+		pass
+
+	def test_delete_contacts(self):
+		phonebk = Phonebook()
+		response = phonebk.delete_contacts('32')
+		self.assertEqual(response['message'], 'contact deleted')
+		pass
+
+	def test_update_contacts(self):
+		phonebk = Phonebook()
+		response = phonebk.update_contacts('038345', 'john')
+		self.assertEqual(response['message'], 'Information updated successfully')
+		pass
+		
+
+
+
